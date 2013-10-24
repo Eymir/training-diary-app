3ec8c59c6736e885739ade875dede4c7912ad4ad
Boris.Shestakov <Boris.Shestakov@gmail.com>
2013-10-24 20:28:29 +0000

    Исправлена бага с перетаскиванием добавлены 3 новых статистики

--- TrainingDiaryGradle/TrainingDiary/src/main/java/myApp/trainingdiary/customview/stat/StatisticEnum.java
+++ TrainingDiaryGradle/TrainingDiary/src/main/java/myApp/trainingdiary/customview/stat/StatisticEnum.java
@@ -11,6 +11,9 @@ import myApp.trainingdiary.R;
  */
 public enum StatisticEnum {
     TRAINING_COUNT(AndroidApplication.getInstance().getString(R.string.tr_count_stat)),
+    FAVORITE_EXERCISE(AndroidApplication.getInstance().getString(R.string.favorite_ex_stat)),
+    TRAINING_DURATION_SUMM(AndroidApplication.getInstance().getString(R.string.tr_dur_sum)),
+    LAST_TRAINING_DAY_COUNT(AndroidApplication.getInstance().getString(R.string.last_tr_time_passed)),
     LAST_TRAINING_DATE(AndroidApplication.getInstance().getString(R.string.last_tr_date_stat));
     private final String desc;
 
--- TrainingDiaryGradle/TrainingDiary/src/main/java/myApp/trainingdiary/customview/stat/StatisticValueFactory.java
+++ TrainingDiaryGradle/TrainingDiary/src/main/java/myApp/trainingdiary/customview/stat/StatisticValueFactory.java
@@ -1,10 +1,15 @@
 package myApp.trainingdiary.customview.stat;
 
+import android.content.res.Resources;
+
 import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 import myApp.trainingdiary.AndroidApplication;
 import myApp.trainingdiary.R;
 import myApp.trainingdiary.db.DBHelper;
+import myApp.trainingdiary.db.entity.Exercise;
 import myApp.trainingdiary.db.entity.TrainingStat;
 
 /**
@@ -14,7 +19,7 @@ public class StatisticValueFactory {
 
     public static StatItem create(StatisticEnum stat) {
         switch (stat) {
-            case LAST_TRAINING_DATE:
+            case LAST_TRAINING_DATE: {
                 DateFormat df = DateFormat.getDateInstance(DateFormat.SHORT);
                 TrainingStat tr_stat = DBHelper.getInstance(null).READ.getLastTrainingStat();
                 String value;
@@ -23,10 +28,39 @@ public class StatisticValueFactory {
                 else
                     value = AndroidApplication.getInstance().getResources().getString(R.string.empty);
                 return new StatItem(stat, value);
+            }
+            case LAST_TRAINING_DAY_COUNT: {
+                TrainingStat tr_stat = DBHelper.getInstance(null).READ.getLastTrainingStat();
+                String value;
+                if (tr_stat != null)
+                    value = toTimeString(System.currentTimeMillis() - tr_stat.getTrainingDate().getTime());
+                else
+                    value = AndroidApplication.getInstance().getResources().getString(R.string.empty);
+                return new StatItem(stat, value);
+            }
             case TRAINING_COUNT:
                 return new StatItem(stat, String.valueOf(DBHelper.getInstance(null).READ.getTrainingCount()));
-
+            case FAVORITE_EXERCISE: {
+                Exercise ex = DBHelper.getInstance(null).READ.getFavoriteExercise();
+                String name = (ex != null) ? ex.getName() : AndroidApplication.getInstance().getResources().getString(R.string.no);
+                return new StatItem(stat, name);
+            }
+            case TRAINING_DURATION_SUMM: {
+                long tr_dur_sum = DBHelper.getInstance(null).READ.getTrainingDurationSumm();
+                String value = toTimeString(tr_dur_sum);
+                return new StatItem(stat, value);
+            }
         }
         return null;
+    }
+
+    private static String toTimeString(long tr_dur_sum) {
+        Calendar c = Calendar.getInstance();
+        c.setTime(new Date(tr_dur_sum));
+        int day = c.get(Calendar.DAY_OF_YEAR) - 1;
+        int hour = c.get(Calendar.HOUR_OF_DAY);
+        int min = c.get(Calendar.MINUTE);
+        Resources res = AndroidApplication.getInstance().getResources();
+        return day + res.getString(R.string.day_short) + " " + hour + res.getString(R.string.hour_short) + " " + min + res.getString(R.string.min_short);
     }
 }
