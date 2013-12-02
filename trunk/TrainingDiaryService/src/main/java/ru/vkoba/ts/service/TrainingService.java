517377e6ba90bc838585b7c14e6a494eee3a0f68
ricoshet666@gmail.com <ricoshet666@gmail.com@5954c3ca-e440-4dbf-9797-71c082081234>
2013-12-02 20:55:39 +0000

    Добавил вывод статистики. Важно! Путь до статистики строго такой: http://localhost:8080/TrainingDiaryService/Statistic, регистрозависимый. Отображение сделано на Spring MVC. Примечание: вывод тестировал на фейковых данных, запросы не проверял, возможны ошибки.

--- trunk/TrainingDiaryService/src/main/java/ru/vkoba/ts/service/TrainingService.java
+++ trunk/TrainingDiaryService/src/main/java/ru/vkoba/ts/service/TrainingService.java
@@ -6,11 +6,12 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.transaction.annotation.Transactional;
 import ru.vkoba.ts.domain.StatisticElement;
 
-import javax.ws.rs.*;
-import javax.ws.rs.core.MediaType;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
-import java.io.File;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * User: vkoba_000
@@ -21,25 +22,32 @@ import java.util.Date;
 @Path("/tds")
 @Scope
 public class TrainingService {
-    JdbcTemplate trainingServiceDao;
+    JdbcTemplate jdbcTemplate;
 
     @POST
     //@Consumes(MediaType.APPLICATION_OCTET_STREAM)
     @Path("/print")
     @Transactional
     public Response addStatistic(StatisticElement element) {
-
         String query = "insert into TrainingStatistic (user_device_uid, trainingstamp_start, trainingstamp_end) values(?,?,?)";
-        trainingServiceDao.update(query, new Object[]{element.getDeviceId(), element.getTrainingStart(), element.getTrainingEnd()});
+        jdbcTemplate.update(query, new Object[]{element.getDeviceId(), element.getTrainingStart(), element.getTrainingEnd()});
         return Response.status(200).entity("OK").build();
     }
 
-
-    public JdbcTemplate getTrainingServiceDao() {
-        return trainingServiceDao;
+    public Map<String, Long> getStatisticByDevicesForPeriod(Date from, Date to) {
+        Map<String, Long> result = new HashMap<String, Long>();
+        return result;
     }
 
-    public void setTrainingServiceDao(JdbcTemplate trainingServiceDao) {
-        this.trainingServiceDao = trainingServiceDao;
+    public long getTotal() {
+        return -1;
+    }
+
+    public JdbcTemplate getJdbcTemplate() {
+        return jdbcTemplate;
+    }
+
+    public void setJdbcTemplate(JdbcTemplate trainingServiceDao) {
+        this.jdbcTemplate = trainingServiceDao;
     }
 }
