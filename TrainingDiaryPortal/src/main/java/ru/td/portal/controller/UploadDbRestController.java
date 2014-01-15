b683431f997df5fe56713cfd6b2406c6d2eab31b
Ricoshet666 <ricoshet666@gmail.com>
2014-01-15 14:56:08 +0000

    Протестировал метод

--- TrainingDiaryPortal/src/main/java/ru/td/portal/controller/UploadDbRestController.java
+++ TrainingDiaryPortal/src/main/java/ru/td/portal/controller/UploadDbRestController.java
@@ -3,14 +3,21 @@ package ru.td.portal.controller;
 import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.*;
 import ru.td.portal.domain.UserData;
 import ru.td.portal.repository.UserDataRepository;
 import ru.td.portal.service.FolderGeneratorService;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -23,28 +30,30 @@ import java.io.IOException;
  * Time: 12:09 PM
  * To change this template use File | Settings | File Templates.
  */
+
 @Controller
 public class UploadDbRestController {
     private Logger log = LoggerFactory.getLogger(UploadDbRestController.class);
     UserDataRepository userDataRepository;
     FolderGeneratorService folderGeneratorService;
 
-
-    @RequestMapping(value = "/uploadDb", method = RequestMethod.POST)
-    public @ResponseBody boolean uploadClientDb(@RequestParam("userData") UserData userData, @RequestParam("file") byte[] file) {
+     //TODO:Костыль с возвращаемым типом, бул почему-то вернуть не получается
+    @RequestMapping(value = "/uploadDb", method = RequestMethod.POST, produces = MediaType.APPLICATION_XML)
+    @ResponseBody
+    public String uploadClientDb(@RequestBody UserData userData) {
         String dbPath = folderGeneratorService.generateFolderPath(userData) + IOUtils.DIR_SEPARATOR + "db.sqlite";
         FileOutputStream fos = null;
         try {
             fos = new FileOutputStream(new File(dbPath));
-            IOUtils.write(file, fos);
+            IOUtils.write(userData.getDb(), fos);
             userDataRepository.addUserData(userData);
         } catch (IOException e) {
-            log.error("Error upload database! Details:",e);
-            return false;
+            log.error("Error upload database! Details:", e);
+            return "ERROR";
         } finally {
             IOUtils.closeQuietly(fos);
         }
-        return true;
+        return "OK";
 
     }
 
