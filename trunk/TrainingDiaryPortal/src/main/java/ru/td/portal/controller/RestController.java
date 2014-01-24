326a71c0b7b9b87dd87f8c93eccc989f94ae8b3d
Ricoshet666@gmail.com <Ricoshet666@gmail.com@5954c3ca-e440-4dbf-9797-71c082081234>
2014-01-24 20:38:10 +0000

    1. Добавил downloadDb 2. Rest-сервисы начинаются с api 3. Правка багов

--- trunk/TrainingDiaryPortal/src/main/java/ru/td/portal/controller/RestController.java
+++ trunk/TrainingDiaryPortal/src/main/java/ru/td/portal/controller/RestController.java
@@ -3,7 +3,7 @@ package ru.td.portal.controller;
 import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import ru.td.portal.domain.UserData;
@@ -12,8 +12,6 @@ import ru.td.portal.service.FolderGeneratorService;
 
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -28,15 +26,18 @@ import java.io.IOException;
  */
 
 @Controller
-public class UploadDbRestController {
-    private Logger log = LoggerFactory.getLogger(UploadDbRestController.class);
+@RequestMapping("/api")
+public class RestController {
+    @Autowired
     UserDataRepository userDataRepository;
+    @Autowired
     FolderGeneratorService folderGeneratorService;
+    private Logger log = LoggerFactory.getLogger(RestController.class);
 
     //TODO:Костыль с возвращаемым типом, бул почему-то вернуть не получается
     @RequestMapping(value = "/uploadDb", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON, consumes = MediaType.APPLICATION_JSON)
-    @ResponseBody
-    public Response uploadClientDb(@RequestBody UserData userData) {
+
+    public @ResponseBody Response uploadClientDb(@RequestBody UserData userData) {
         String dbPath = folderGeneratorService.generateFolderPath(userData) + IOUtils.DIR_SEPARATOR + "db.sqlite";
         FileOutputStream fos = null;
         try {
@@ -54,10 +55,9 @@ public class UploadDbRestController {
     }
 
     @RequestMapping(value = "/downloadDb", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON, consumes = MediaType.APPLICATION_JSON)
-    @ResponseBody
-    public Response downloadClientDb(@RequestParam("id")String id, @RequestParam("channel")String channel) {
+    public  @ResponseBody Response downloadClientDb(@RequestParam("id") String id,@RequestParam("channel") String channel) {
         UserData result = userDataRepository.getUserDataByRegIdAndChannel(id, channel);
-
+        checkUserDataNotNull(id, channel, result);
         FileInputStream fis = null;
         try {
             fis = new FileInputStream(new File(result.getDbPath()));
@@ -65,11 +65,17 @@ public class UploadDbRestController {
             return Response.status(200).entity(result).build();
         } catch (IOException e) {
             log.error("Error upload database! Details:", e);
-            return Response.status(500).entity("Error").build();
+            return Response.status(500).entity(e).build();
         } finally {
             IOUtils.closeQuietly(fis);
         }
 
+    }
+
+    private void checkUserDataNotNull(String id, String channel, UserData result) {
+        if (result == null || result.getDbPath() == null){
+            throw new RuntimeException("Error! Record with id="+id+" and channel="+channel+" not found");
+        }
     }
 
     public FolderGeneratorService getFolderGeneratorService() {
