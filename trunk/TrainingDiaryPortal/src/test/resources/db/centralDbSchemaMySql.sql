b07d15e4053ca0ae675adc6d4919fb86fc98fd94
Boris.Shestakov@gmail.com <Boris.Shestakov@gmail.com@5954c3ca-e440-4dbf-9797-71c082081234>
2014-01-28 17:35:09 +0000

    Версия скриптов схемы для наката с нуля

---
+++ trunk/TrainingDiaryPortal/src/test/resources/db/centralDbSchemaMySql.sql
@@ -0,0 +1,3 @@
+CREATE TABLE FolderGenerator (counter integer);
+
+CREATE TABLE UserData (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY , registration_id varchar(100),registration_channel varchar(30), email varchar(70), db_path varchar(150))
