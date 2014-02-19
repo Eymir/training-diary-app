efaeea58e73d88f5e67c0c590dd3a047cb932540
Ricoshet666 <ricoshet666@gmail.com>
2014-02-19 20:35:43 +0000

    Добавил декодирование Base64

--- TrainingDiaryPortal/src/main/java/ru/td/portal/controller/RestController.java
+++ TrainingDiaryPortal/src/main/java/ru/td/portal/controller/RestController.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.apache.commons.codec.binary.Base64;
 import ru.td.portal.domain.UserData;
 import ru.td.portal.repository.UserDataRepository;
 import ru.td.portal.service.FolderGeneratorService;
@@ -51,7 +52,7 @@ public class RestController {
         FileOutputStream fos = null;
         try {
             fos = new FileOutputStream(new File(dbPath));
-            IOUtils.write(userDataFromDb.getDb(), fos, CharEncoding.UTF_8);
+            IOUtils.write(Base64.decodeBase64(userDataFromDb.getDb()), fos);
             userDataFromDb.setDbPath(dbPath);
             userDataRepository.saveUserData(userDataFromDb);
         } catch (IOException e) {
