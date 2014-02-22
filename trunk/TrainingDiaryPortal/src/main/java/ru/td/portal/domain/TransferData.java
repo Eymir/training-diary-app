0af5bb1cba53e2408612f0b564c17428ba91d263
Boris.Shestakov@gmail.com <Boris.Shestakov@gmail.com@5954c3ca-e440-4dbf-9797-71c082081234>
2014-02-22 15:53:49 +0000

    добавил объект транспорта

---
+++ trunk/TrainingDiaryPortal/src/main/java/ru/td/portal/domain/TransferData.java
@@ -0,0 +1,27 @@
+package ru.td.portal.domain;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * User: boris_dev
+ * Date: 1/14/14
+ * Time: 3:58 PM
+ */
+
+
+public class TransferData {
+    public static final String REG_CHANNEL = "registration_channel";
+    public static final String REG_ID = "registration_id";
+    public static final String DATABASE_STRING = "database_string";
+
+    private Map<String, Object> map;
+
+    public Map<String, Object> getMap() {
+        if (map == null) {
+            return new HashMap<String, Object>();
+        }
+        return map;
+    }
+}
