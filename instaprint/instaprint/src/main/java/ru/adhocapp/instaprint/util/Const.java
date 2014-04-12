7a9fdddbb7e49fa0902aba3ab7c4af2672ffe8b8
svmalugin <svmalugin@gmail.com>
2014-04-12 00:16:24 +0000

    Отправка заказов через яндекс почту

---
+++ instaprint/instaprint/src/main/java/ru/adhocapp/instaprint/util/Const.java
@@ -0,0 +1,15 @@
+package ru.adhocapp.instaprint.util;
+
+import android.support.v7.appcompat.R;
+
+/**
+ * Created by root on 12.04.14.
+ */
+public class Const {
+
+    public static String MAIL_ACCOUNT = "ru.adhocapp.instaprint@yandex.ru";
+    public static String MAIL_PASSWORD = "instaprint2163927";
+    public static String LOG_TAG = "instaprint";
+
+
+}
---
+++ instaprint/instaprint/src/main/java/ru/adhocapp/instaprint/util/mail/MailHelper.java
@@ -0,0 +1,56 @@
+package ru.adhocapp.instaprint.util.mail;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import ru.adhocapp.instaprint.util.Const;
+
+public class MailHelper  {
+
+    private static MailHelper instance;
+    private String title;
+    private String body;
+    private String attach;
+
+    public MailHelper(String t,String b,String a)  {
+        title = t;
+        body = b;
+        attach = a;
+    }
+
+    public static MailHelper getInstance(String t, String b, String a){
+        if (instance == null){
+            instance = new MailHelper(t,b,a);
+        }
+        return instance;
+    }
+
+    public void sendMail(){
+        senderMailAsync async_sending = new senderMailAsync();
+	    async_sending.execute();
+    }
+
+    private class senderMailAsync extends AsyncTask<Object, String, Boolean> {
+
+		@Override
+		protected void onPreExecute() {
+		}
+		
+		@Override
+		protected void onPostExecute(Boolean result) {
+		}
+
+		@Override
+		protected Boolean doInBackground(Object... params) {
+
+			try {
+                MailSenderClass sender = new MailSenderClass(Const.MAIL_ACCOUNT, Const.MAIL_PASSWORD);
+                sender.sendMail(title, body, Const.MAIL_ACCOUNT, Const.MAIL_ACCOUNT, attach);
+			} catch (Exception e) {
+                Log.e(Const.LOG_TAG, e.toString());
+			}
+			
+			return false;
+		}
+	}
+}
