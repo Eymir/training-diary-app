876a3ab571f97b3d1848537785657f33e777bc82
Boris.Shestakov@gmail.com <Boris.Shestakov@gmail.com@5954c3ca-e440-4dbf-9797-71c082081234>
2014-04-12 01:07:53 +0000

    Добавлены статусы заказов

--- trunk/instaprint/instaprint/src/main/java/ru/adhocapp/instaprint/db/entity/OrderStatus.java
+++ trunk/instaprint/instaprint/src/main/java/ru/adhocapp/instaprint/db/entity/OrderStatus.java
@@ -4,4 +4,20 @@ package ru.adhocapp.instaprint.db.entity;
  * Created by Lenovo on 12.04.2014.
  */
 public enum OrderStatus {
+    /**
+     * Заполнение обязательных полей
+     */
+    CREATING,
+    /**
+     * Оплата
+     */
+    PAYING,
+    /**
+     * Посылка
+     */
+    SENDING,
+    /**
+     * Исполнен
+     */
+    EXECUTED
 }
